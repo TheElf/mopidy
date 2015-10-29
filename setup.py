ce02961310c213f89a9b155af1a64abc0bb27727
Stein Magnus Jodal <stein.magnus@jodal.no>
2015-10-29 22:01:58 +0100

    py3: Add Python 3.4+ support

--- setup.py
+++ setup.py
@@ -52,6 +52,9 @@ setup(
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Multimedia :: Sound/Audio :: Players',
     ],
 )
